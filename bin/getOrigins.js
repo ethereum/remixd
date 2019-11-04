890f23cf4184d9c5344ebbda822d0d31db61a437
Edi Sinovčić <edi.sinovcic@gmail.com>
2019-11-04 15:49:12 +0100

    Using standard to fix indentation problems

--- bin/getOrigins.js
+++ bin/getOrigins.js
@@ -1,22 +1,22 @@
-const https = require('https');
-const fs = require('fs');
+const https = require('https')
+const fs = require('fs')
 
-function fetch(url) {
-    return new Promise((resolve, reject) => {
-        https.get(url, (res) => {
-            let result = '';
-            res.on('data', (data) => result += data);
-            res.on('end', () => resolve(JSON.parse(result)));
-            res.on('error', err => reject(err));
-        })
+function fetch (url) {
+  return new Promise((resolve, reject) => {
+    https.get(url, (res) => {
+      let result = ''
+      res.on('data', (data) => result += data)
+      res.on('end', () => resolve(JSON.parse(result)))
+      res.on('error', err => reject(err))
     })
+  })
 }
 
 fetch('https://raw.githubusercontent.com/ethereum/remix-plugin/master/projects/client/assets/origins.json').then(result => {
-    fs.writeFile("public/origins.json", JSON.stringify(result), function(err) {
-        if(err) {
-            return console.log(err);
-        }
-        console.log("The file was saved!");
-    });
-});
+  fs.writeFile('public/origins.json', JSON.stringify(result), function (err) {
+    if (err) {
+      return console.log(err)
+    }
+    console.log('The file was saved!')
+  })
+})
--- src/errors/errors.js
+++ src/errors/errors.js
@@ -1,12 +1,12 @@
 class UnsuportedCommandError extends Error {
-    constructor(message) {
-        super();
-        this.message = message;
-        this.code = 404;
-        this.type = "UnsupportedCommandError";
-    }
+  constructor (message) {
+    super()
+    this.message = message
+    this.code = 404
+    this.type = 'UnsupportedCommandError'
+  }
 }
 
 module.exports = {
-    UnsuportedCommandError
+  UnsuportedCommandError
 }
