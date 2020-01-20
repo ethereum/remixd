dc44df35d2342e0cff6cf199db42d591a776673f
Edi Sinovcic <edi.sinovcic@gmail.com>
2020-01-20 13:16:11 +0100

    Updating config

--- src/services/git.js
+++ src/services/git.js
@@ -1,26 +1,25 @@
-const path = require('path')
 const ProcessManager = require('../utils/processManager')
 const {validateCommand} = require('../utils/folders')
 const gitRegex = '^git\\s[^&|;]*$'
 
 class GitService {
 
-    constructor() {
-        this.processManager = new ProcessManager()
-        this.websocket = null
-        this.currentSharedFolder = null
-    }
+  constructor() {
+    this.processManager = new ProcessManager()
+    this.websocket = null
+    this.currentSharedFolder = null
+  }
 
-    setWebSocket(websocket) {
-        this.websocket = websocket
-    }
+  setWebSocket(websocket) {
+    this.websocket = websocket
+  }
 
-    sharedFolder(currentSharedFolder, readOnly) {
-        this.currentSharedFolder = currentSharedFolder
-        this.readOnly = readOnly
-    }
+  sharedFolder(currentSharedFolder, readOnly) {
+    this.currentSharedFolder = currentSharedFolder
+    this.readOnly = readOnly
+  }
 
-    /**
+  /**
      * Create and check if command is valid
      * @param action
      * @param id
@@ -29,11 +28,11 @@ class GitService {
      * @param payload
      * @param callback
      */
-    command(args, callback) {
-        validateCommand(args.script, gitRegex)
-        const options = {cwd: this.currentSharedFolder, shell: true}
-        this.processManager.spawnProcess(args.script, options, callback)
-    }
+  command(args, callback) {
+    validateCommand(args.script, gitRegex)
+    const options = {cwd: this.currentSharedFolder, shell: true}
+    this.processManager.spawnProcess(args.script, options, callback)
+  }
 
 }
 
